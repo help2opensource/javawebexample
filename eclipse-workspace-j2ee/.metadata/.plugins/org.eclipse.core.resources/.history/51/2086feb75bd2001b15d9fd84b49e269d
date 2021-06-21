bd2d74dd499f0ffd5c4931218d331086bb44b80e
help2opensource <help2opensource@gmail.com>
2021-06-21 00:03:31 -0700

    all all

---
+++ eclipse-workspace-j2ee/.metadata/.plugins/org.eclipse.core.resources/.history/51/2086feb75bd2001b15d9fd84b49e269d
@@ -0,0 +1,14 @@
+<%@page import="java.util.ArrayList"%>
+ 
+<html>
+<head>
+<title>Java Code Geeks Snippets - Sample JSP Page</title>
+<meta>
+</meta>
+</head>
+ 
+<body>
+    <br /> 
+    Current date is: <%=new java.util.Date()%>
+</body>
+</html>
---
+++ eclipse-workspace-j2ee/examples/src/org/examples/ShopCartServlet.java
@@ -0,0 +1,17 @@
+package org.examples;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+class ShopCartServlet extends HttpServlet
+{
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+    {
+        // Implement the shop cart functionality.
+    }
+    
+   
+}
+
